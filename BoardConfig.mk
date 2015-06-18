cf6e21039c18da8a50731edf15f495752132a723
sakuraba <sakura.bar.com@gmail.com>
2015-06-18 22:06:41 +0900

    add support d2kdi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,13 +24,13 @@
 -include device/samsung/msm8960-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can
+TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can,d2kdi
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Kernel
-TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
+TARGET_KERNEL_CONFIG        := cyanogen_d2kdi_defconfig
 
 # Audio
 BOARD_HAVE_AUDIENCE_A2220 := true
--- init/init_d2lte.c
+++ init/init_d2lte.c
@@ -84,6 +84,14 @@ void vendor_load_properties()
         property_set("ro.product.model", "SC-06D");
         property_set("ro.product.device", "d2dcm");
         property_set("ro.ril.enable.dcm.feature", "1");
+    } else if (strstr(bootloader, "SCL21")) {
+        /* d2kdi */
+        cdma_properties("0", "10", "44054", "KDDI", "1");
+        property_set("ro.build.fingerprint", "KDDI/SCL21/SCL21:4.1.2/JZO54K/SCL21KDBND1:user/release-keys");
+        property_set("ro.build.description", "d2kdi-user 4.1.2 JZO54K SCL21KDBND1 release-keys");
+        property_set("ro.product.model", "SCL21");
+        property_set("ro.product.device", "SCL21");
+        property_set("ro.com.google.clientidbase", "android-samsung");
     }  else if (strstr(bootloader, "R530M")) {
         /* d2mtr */
         cdma_properties("0", "8", "311660", "MetroPCS", "0");
